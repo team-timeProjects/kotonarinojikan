6fd3d844e524a0f16782132d3c475c0365693a12
中川文仁 <koiyaa2020@gmail.com>
2015-05-07 19:52:58 +0900

    デザイン更新

--- source/Clock.h
+++ source/Clock.h
@@ -16,22 +16,23 @@ private:
 		POINT		start;			//	元の位置
 		POINT		end;			//	移動後の位置
 		POINT		shift;			//	カメラ用ずれ幅
+		float		speed;			//	速度
+
 		float		minuteAngle;	//	分針の向き
 		float		hourAngle;		//	時針の向き
-		float		speed;			//	速度
 	};
 
 private:
-		//	パラメータ
-		static	const	int		CLOCK_MAX = 4;
-		CLOCK_OBJ	obj[4];		//	時計構造体
-		POINT		length;		//	距離
-		float			org_speed;	//	元のスピード
-		float			cul_speed;	//	現在選択中のそもそものスピード
-		int			selectNum;	//	選択中の番号
-		bool			clear;		//	クリア
-		float			t;			//	割合
-		int			step;		//	ステップ
+	//	パラメータ
+	static	const	int		CLOCK_MAX = 4;
+	CLOCK_OBJ	obj[4];		//	時計構造体
+	POINT		length;		//	距離
+	float		org_speed;	//	元のスピード
+	float		cul_speed;	//	現在選択中のそもそものスピード
+	int			selectNum;	//	選択中の番号
+	bool		clear;		//	クリア
+	float		t;			//	割合
+	int			step;		//	ステップ
 
 private:
 	//	オブジェクト
@@ -42,29 +43,29 @@ private:
 
 public:
 	//	初期化・解放
-	Clock( void );
-	~Clock( void );
+	Clock(void);
+	~Clock(void);
 
 	//　初期化・読み込み
-	bool	Initialize( void );
-	void	InitObj( void );		//	オブジェクト初期化
-	void	SetObj( int n, int x, int y, float hAngle, float mAngle, float s );
+	bool	Initialize(void);
+	void	InitObj(void);		//	オブジェクト初期化
+	void	SetObj(int n, int x, int y, float hAngle, float mAngle, float s);
 
 	//　更新
-	void	Update( void );
+	void	Update(void);
 
 	//	描画
-	void	Render( void );
+	void	Render(void);
 
 	//　MAIN_STATE_GAME動作関数
-	bool	CheckWithin( const POINT& p, const int& startX, const int& startY, const int size );	//	範囲内かどうか調べる
-	void	Lerp( POINT& out, const POINT p1, const POINT p2, float t );	//	POINT補間
-	void	Move( void );		//	動作
-	void	Control( void );	//	操作
-	void	CalcPos( void );	//	位置関係計算
-	
+	void	Move(void);		//	動作
+	bool	CheckWithin(const POINT& p, const int& startX, const int& startY, const int size);	//	範囲内かどうか調べる
+	void	Lerp(POINT& out, const POINT p1, const POINT p2, float t);	//	POINT補間
+	void	Control(void);	//	操作
+	void	CalcPos(void);	//	位置関係計算
+
 	//	情報取得・設定
-	void	GetTitleInfo( float hAngle, float mAngle );
+	void	GetTitleInfo(float hAngle, float mAngle);
 };
 
 //****************************************************************************************
--- source/Metronom.h
+++ source/Metronom.h
@@ -12,14 +12,15 @@ class Metronom
 private:
 	struct METRONOM
 	{
-		POINT		pos;			//	位置
+		POINT		pos;		//	位置
 		POINT		start;		//	元の位置
-		POINT		end;			//	移動後の位置
+		POINT		end;		//	移動後の位置
 		POINT		shift;		//	カメラ用ずれ幅
-		float			speed;		//	速度
-		float			param;		//	回転用
-		float			height;		//	高さ
-		float			angle;		//	向き
+		float		speed;		//	速度
+
+		float		param;		//	回転用
+		float		height;		//	高さ
+		float		angle;		//	向き
 	};
 
 private:
@@ -29,39 +30,39 @@ private:
 	iex2DObj*		back;
 	iex2DObj*		metronom;
 	iex2DObj*		needle;
-	
+
 	//	パラメータ
-	POINT		length;			//	距離
-	float			org_speed;	//	元のスピード
-	float			cul_speed;	//	現在選択中のそもそものスピード
+	POINT		length;		//	距離
+	float		org_speed;	//	元のスピード
+	float		cul_speed;	//	現在選択中のそもそものスピード
 	int			selectNum;	//	選択中の番号
-	bool			clear;			//	クリア
-	float			t;					//	割合
-	int			step;			//	ステップ
-	float			plus_speed;	//	スピード加算値
+	bool		clear;		//	クリア
+	float		t;			//	割合
+	int			step;		//	ステップ
+	float		plus_speed;	//	スピード加算値
 
 public:
 	//	初期化・解放
-	Metronom( void );
-	~Metronom( void );
-	void	Initialize( void );
-	void	SetObj( int n, int x, int y, float height, float speed );
+	Metronom(void);
+	~Metronom(void);
+	void	Initialize(void);
+	void	SetObj(int n, int x, int y, float height, float speed);
 
 	//	更新
-	void	Update( void );
+	void	Update(void);
 
 	//	描画
-	void	Render( void );
+	void	Render(void);
 
 	//	動作関数
-	void	Move( void );
-	bool	CheckWithin( const POINT& p, const int& startX, const int& startY, const int size );	//	範囲内かどうか調べる
-	void	Lerp( POINT& out, const POINT p1, const POINT p2, float t );	//	POINT補間
-	void	Control( void );	//	操作
-	void	CalcPos( void );	//	位置関係計算
+	void	Move(void);
+	bool	CheckWithin(const POINT& p, const int& startX, const int& startY, const int size);	//	範囲内かどうか調べる
+	void	Lerp(POINT& out, const POINT p1, const POINT p2, float t);	//	POINT補間
+	void	Control(void);	//	操作
+	void	CalcPos(void);	//	位置関係計算
 
 	//	情報取得・設定
-	void	SetTitleInfo( float angle );
+	void	SetTitleInfo(float angle);
 };
 
 //****************************************************************************************
